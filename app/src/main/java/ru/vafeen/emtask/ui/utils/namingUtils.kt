3cf9a45b59787a621b4327fa4c5d95a4d2c6260e
Vafeen <666av6@gmail.com>
2024-09-19 23:41:28 +0300

    Add to naming utils vacancies russian word depends of count and localdate

--- app/src/main/java/ru/vafeen/emtask/ui/utils/namingUtils.kt
+++ app/src/main/java/ru/vafeen/emtask/ui/utils/namingUtils.kt
@@ -1,11 +1,34 @@
 package ru.vafeen.emtask.ui.utils
 
 import java.time.LocalDate
+import java.time.Month
 
 fun generatePeopleViewStringByCount(count: Int): String = "Сейчас просматривает $count человек"
 
 fun generatePublishedDateByLocalDate(localDate: LocalDate): String =
-    "Опубликовано ${localDate.dayOfWeek}.${localDate.month}"
+    "Опубликовано ${localDate.dayOfMonth} ${
+        when (localDate.month) {
+            Month.JANUARY -> "января"
+            Month.FEBRUARY -> "февраля"
+            Month.MARCH -> "марта"
+            Month.APRIL -> "апреля"
+            Month.MAY -> "мая"
+            Month.JUNE -> "июня"
+            Month.JULY -> "июля"
+            Month.AUGUST -> "августа"
+            Month.SEPTEMBER -> "сентября"
+            Month.OCTOBER -> "октября"
+            Month.NOVEMBER -> "ноября"
+            Month.DECEMBER -> "декабря"
+            else -> ""
+        }
+    }"
 
-fun generateMoreCountOfVacanciesByCount(count: Int): String =
-    "Еще ${if (count > 2) count - 2 else 0} вакансии"
+fun generateMoreCountOfVacanciesByCount(count: Int): String = "Еще $count ${
+    when {
+        count in 11..19 -> "вакансий"
+        count % 10 == 1 -> "вакансия"
+        count % 10 in 2..4 -> "вакансии"
+        else -> "вакансий"
+    }
+}"
