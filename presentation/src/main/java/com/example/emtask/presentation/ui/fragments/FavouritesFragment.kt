5ec482a4341c44f2623849e2486999e5d712f601
Vafeen <666av6@gmail.com>
2024-10-16 05:46:50 +0300

    Add logic to FavouritesFragment

--- presentation/src/main/java/com/example/emtask/presentation/ui/fragments/FavouritesFragment.kt
+++ presentation/src/main/java/com/example/emtask/presentation/ui/fragments/FavouritesFragment.kt
@@ -8,14 +8,44 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.emtask.presentation.databinding.FragmentFavouritesBinding
+import com.example.emtask.presentation.noui.utils.generateMoreCountOfVacanciesByCount
+import com.example.emtask.presentation.ui.components.modifiers.CustomItemDecoration
+import com.example.emtask.presentation.ui.components.modifiers.CustomLinearLayoutManager
+import com.example.emtask.presentation.ui.viewmodels.FavouritesFragmentViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavouritesFragment : Fragment() {
     private lateinit var binding: FragmentFavouritesBinding
+    private val vModel: FavouritesFragmentViewModel by viewModel()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = FragmentFavouritesBinding.inflate(inflater, container, false)
+        binding.favouritesListview.setFavouritesAdapterSettings()
+        vModel.collectDataFromDB { size ->
+            binding.numberOfFavourites.text =
+                generateMoreCountOfVacanciesByCount(count = size) { vacanciesWord ->
+                    "$size $vacanciesWord"
+                }
+        }
         return binding.root
     }
 
+    private fun RecyclerView.setFavouritesAdapterSettings() {
+        layoutManager = CustomLinearLayoutManager(
+            context = requireContext(),
+            orientation = LinearLayoutManager.VERTICAL,
+            reverseLayout = false,
+            canScrollVertically = true,
+            canScrollHorizontally = false
+        )
+        adapter = vModel.favouritesAdapter
+        addItemDecoration(
+            CustomItemDecoration(
+                space = 16,
+                orientation = CustomItemDecoration.CustomItemDecorationOrientation.VERTICAL
+            )
+        )
+
+    }
 }
--- presentation/src/main/java/com/example/emtask/presentation/ui/viewmodels/FavouritesFragmentViewModel.kt
+++ presentation/src/main/java/com/example/emtask/presentation/ui/viewmodels/FavouritesFragmentViewModel.kt
@@ -1,7 +1,41 @@
 package com.example.emtask.presentation.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.emtask.data.database.DatabaseRepository
+import com.example.emtask.data.database.entity.VacancyEntity
+import com.example.emtask.presentation.noui.AppCoroutineDispatchers
+import com.example.emtask.presentation.ui.VacationClickListener
+import com.example.emtask.presentation.ui.components.adapters.FavouritesAdapter
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
-class FavouritesFragmentViewModel : ViewModel() {
+class FavouritesFragmentViewModel(
+    private var databaseRepository: DatabaseRepository,
+    private val appCoroutineDispatchers: AppCoroutineDispatchers,
+) : ViewModel(), VacationClickListener {
+
+    val favouritesAdapter: FavouritesAdapter = FavouritesAdapter(vacationClickListener = this)
+
+
+    fun collectDataFromDB(onShowData: (Int) -> Unit) {
+        viewModelScope.launch(appCoroutineDispatchers.io) {
+            databaseRepository.getAllVacancy().collect { listVacancyEntity ->
+                favouritesAdapter.favourites = listVacancyEntity.filter { vacancyEntity ->
+                    vacancyEntity.isFavorite
+                }
+                withContext(appCoroutineDispatchers.main) {
+                    favouritesAdapter.notifyDataSetChanged()
+                    onShowData(favouritesAdapter.favourites.size)
+                }
+            }
+        }
+    }
+
+    override fun onClickUpdateVacancy(vacancyEntity: VacancyEntity) {
+        viewModelScope.launch(appCoroutineDispatchers.io) {
+            databaseRepository.insertAllVacancy(vacancyEntity)
+        }
+    }
 }
