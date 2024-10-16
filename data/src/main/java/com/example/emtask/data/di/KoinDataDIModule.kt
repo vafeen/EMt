9e1a1c99618312e99d37a2c5e56fd18dc235f1af
Vafeen <666av6@gmail.com>
2024-10-16 05:42:23 +0300

    Modify koinDataDIModule

--- data/src/main/java/com/example/emtask/data/di/KoinDataDIModule.kt
+++ data/src/main/java/com/example/emtask/data/di/KoinDataDIModule.kt
@@ -1,16 +1,25 @@
-package com.example.emtask.data.network.di
+package com.example.emtask.data.di
 
+import androidx.room.Room
+import com.example.emtask.data.database.AppDatabase
+import com.example.emtask.data.database.DatabaseInfo
 import com.example.emtask.data.network.GDriveService
 import com.example.emtask.data.network.GDriveServiceLink
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-val koinNetworkDIModule = module {
+val koinDataDIModule = module {
     single<GDriveService> {
         Retrofit.Builder()
             .baseUrl(GDriveServiceLink.BASE_LINK)
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(GDriveService::class.java)
+    }
+    single<AppDatabase> {
+        Room
+            .databaseBuilder(androidContext(), AppDatabase::class.java, DatabaseInfo.NAME)
+            .build()
     }
 }
