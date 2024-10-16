db9c939cc2646e872d1b6a1bd2c07c9ff426f4a2
Vafeen <666av6@gmail.com>
2024-10-16 05:42:39 +0300

    Add OffersAdapter to ServicesModule

--- presentation/src/main/java/com/example/emtask/presentation/noui/di/koinServicesModule.kt
+++ presentation/src/main/java/com/example/emtask/presentation/noui/di/koinServicesModule.kt
@@ -1,9 +1,11 @@
 package com.example.emtask.presentation.noui.di
 
 import com.example.emtask.presentation.noui.AppCoroutineDispatchers
+import com.example.emtask.presentation.ui.components.adapters.OffersAdapter
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val koinServicesModule = module {
     singleOf(::AppCoroutineDispatchers)
+    singleOf(::OffersAdapter)
 }
